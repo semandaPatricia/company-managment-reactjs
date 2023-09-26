94699e9921e59abf02e71f1a0250b700503dafb1
semanda <patash248@gmail.com>
2023-09-26 12:19:09 +0200

    added modal popup and a form in the input body

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,43 +1,44 @@
 import { useState } from "react";
 import Employee from "./components/Employee";
 import { v4 as uuidv4 } from "uuid";
+import EmployeeModal from "./components/EmployeeModal";
 
 const employeeInfos = [
   {
     id: "1",
     name: "Ariana",
     role: "Entertainer",
-    img: "https://unsplash.com/photos/a-man-with-glasses-sitting-on-a-table-Lgitb85y7-A",
+    img: "https://images.pexels.com/photos/5393594/pexels-photo-5393594.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
   },
   {
     id: "2",
     name: "patrick",
     role: "Developer",
-    img: "https://unsplash.com/photos/a-man-with-glasses-sitting-on-a-table-Lgitb85y7-A",
+    img: "https://images.pexels.com/photos/697509/pexels-photo-697509.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
   },
   {
     id: "3",
     name: "Attila",
     role: "project managment",
-    img: "https://unsplash.com/photos/a-man-with-glasses-sitting-on-a-table-Lgitb85y7-A",
+    img: "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
   },
   {
     id: "4",
-    name: "whittaker",
+    name: "lizzie",
     role: "Ux Designer",
-    img: "https://unsplash.com/photos/a-man-with-glasses-sitting-on-a-table-Lgitb85y7-A",
+    img: "https://images.pexels.com/photos/5876695/pexels-photo-5876695.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
   },
   {
     id: "5",
-    name: "Joshua",
+    name: "Jo Hyn",
     role: "DevOps",
-    img: "https://unsplash.com/photos/a-man-with-glasses-sitting-on-a-table-Lgitb85y7-A",
+    img: "https://images.pexels.com/photos/3454298/pexels-photo-3454298.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
   },
   {
     id: "6",
     name: "fallon",
     role: "scrum master",
-    img: "https://unsplash.com/photos/a-man-with-glasses-sitting-on-a-table-Lgitb85y7-A",
+    img: "https://images.pexels.com/photos/5393594/pexels-photo-5393594.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
   },
 ];
 
@@ -46,20 +47,17 @@ function App() {
 
   return (
     <div className=" grid grid-cols-3 gap-2ą">
-      {employeeInfos.map((employeeInfo)=> {
+      {employeeInfos.map((employeeInfo) => {
         return (
-         
-            <Employee
+          <Employee
             key={employeeInfo.id}
-              name={employeeInfo.name}
-              role={employeeInfo.role}
-              img={employeeInfo.img}
-            />
-          
-        ) 
-
-      
+            name={employeeInfo.name}
+            role={employeeInfo.role}
+            img={employeeInfo.img}
+          />
+        );
       })}
+      <EmployeeModal />
     </div>
   );
 }
--- client/src/components/EmployeeModal.jsx
+++ client/src/components/EmployeeModal.jsx
@@ -1,37 +1,95 @@
 import React from "react";
 
-const EmployeeModal = (props) => {
+
+const EmployeeModal = ({ show }) => {
+  
   return (
-    <div class="modal" tabindex="-1">
-      <div class="modal-dialog">
-        <div class="modal-content">
-          <div class="modal-header">
-            <h5 class="modal-title">Modal title</h5>
+    <>
+     {
+     show ?
+    
+    <div className="modal" tabindex="-1">
+      <div className="modal-dialog">
+        <div className="modal-content">
+          <div className="modal-header">
+            <h5 className="modal-title">Modal title</h5>
             <button
               type="button"
-              class="btn-close"
+              className="btn-close"
               data-bs-dismiss="modal"
               aria-label="Close"
             ></button>
           </div>
-          <div class="modal-body">
-            <p>Modal body text goes here.</p>
-          </div>
-          <div class="modal-footer">
+      <div className="modal-body">
+            <p>
+              {" "}
+              body goes here !!!!
+              <form action="" className="w-full max-w-sm" >
+                <div  className="md:flex md:items-center mb-6">
+                  <div className="md:w-1/3">
+                    <label
+                      className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
+                      htmlFor="inline-full-name"
+                    >
+                      Full Name
+                    </label>
+                  </div>
+                  <div class="md:w-2/3">
+                    <input
+                      className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+                      id="inline-full-name"
+                      type="text"
+                      value="Jane Doe"
+                    />
+                  </div>
+
+                  <div className="md:w-1/3">
+                    <label
+                      className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
+                      htmlFor="inline-full-name"
+                    >
+                      role
+                    </label>
+                  </div>
+                  <div className="md:w-2/3">
+                    <input
+                      className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+                      id="inline-full-name"
+                      type="text"
+                      value="role"
+                    />
+                  </div>
+
+                  <div className="md:w-2/3">
+                    <button
+                      className="shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded"
+                      type="button"
+                    >
+                      Edit
+                    </button>
+                  </div>
+                </div>
+              </form>
+            </p>
+     </div>
+          <div className="modal-footer">
             <button
               type="button"
-              class="btn btn-secondary"
+              className="btn btn-secondary"
               data-bs-dismiss="modal"
             >
               Close
             </button>
-            <button type="button" class="btn btn-primary">
+            <button type="button" className="btn btn-primary">
               Save changes
             </button>
           </div>
         </div>
       </div>
     </div>
+      : null
+    }
+    </>
   );
 };
 
